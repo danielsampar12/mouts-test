ff05c961b751a88d9b96ecf9722129e8f97f820b
Daniel Sampar <daniel.sampar12@gmail.com>
2022-05-11 16:58:56 -0300

    NavBar test

---
+++ src/components/Navigations/components/NavBar/NavBar.test.tsx
@@ -0,0 +1,27 @@
+import { render, fireEvent, act } from "@testing-library/react";
+import { RouterContext } from "next/dist/shared/lib/router-context";
+
+import { NavBar } from ".";
+import { createMockRouter } from "../../../../tests/createMockRouter";
+
+describe("FavoriteButton", () => {
+  it("Should navigate correctly", () => {
+    const push = jest.fn();
+
+    const { getByText } = render(
+      <RouterContext.Provider value={createMockRouter({ push })}>
+        <NavBar />
+      </RouterContext.Provider>
+    );
+
+    expect(getByText("Home")).toBeInTheDocument();
+
+    fireEvent.click(getByText("Home"));
+
+    expect(push).toHaveBeenCalledWith("/");
+
+    fireEvent.click(getByText("Favoritos"));
+
+    expect(push).toHaveBeenCalledWith("/favorites");
+  });
+});
--- src/components/Navigations/components/NavBar/index.tsx
+++ src/components/Navigations/components/NavBar/index.tsx
@@ -3,12 +3,12 @@ import { useRouter } from "next/router";
 import * as S from "./styles";
 
 export function NavBar() {
-  const { push } = useRouter();
+  const router = useRouter();
 
   return (
     <S.Container>
-      <S.NavLink onClick={() => push("/")}>Home</S.NavLink>
-      <S.NavLink onClick={() => push("/favorites")}>Favoritos</S.NavLink>
+      <S.NavLink onClick={() => router.push("/")}>Home</S.NavLink>
+      <S.NavLink onClick={() => router.push("/favorites")}>Favoritos</S.NavLink>
     </S.Container>
   );
 }
